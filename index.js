e61d6e3c778b9187ecb5667b9c4c0cea93ae1653
guilhermehd903 <guilhermegdrag@gmail.com>
2025-10-02 18:46:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const app = express();
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.kinghost.net", 
+  port: 587,
+  secure: false, 
+  auth: {
+    user: "portal@unimble.com.br",
+    pass: "TeamUN@2025",
+  },
+});
+
+app.post("/send-email", async (req, res) => {
+  const { to, subject, html } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: `"Portal Unimble" <portal@unimble.com.br>`,
+      to,
+      subject,
+      html,
+    });
+
+    res.json({ success: true, message: "E-mail enviado com sucesso" });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
